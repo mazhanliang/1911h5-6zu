293053b6477a3372e4c04cf017b8698cb3642cd1
whizz2000 <2756571228@qq.com>
2020-08-17 10:59:46 +0800

    h5模板

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Index from '@/components/Index'
+import New from '@/components/New'
+import Category from '@/components/Category'
+import Search from '@/components/Search'
+import Full from '@/components/Full'
+import Error from '@/components/Error'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +15,36 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/Index',
+      name: 'Index',
+      component: Index
+    },
+    {
+    path: '/Category',
+    name: 'Category',
+    component: Category
+    },
+    {
+      path: '/Search',
+      name: 'Search',
+      component: Search
+    },
+    {
+      path: '/Full',
+      name: 'Full',
+      component: Full
+    },
+    {
+      path: '/New',
+      name: 'New',
+      component: New
+    },
+    {
+      path: '/Error',
+      name: 'Error',
+      component: Error
     }
   ]
 })
